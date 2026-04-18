161bbc21d88b705814079c7eb42b34495e19d34c
Jordan Oos <141826168+ZealousGem@users.noreply.github.com>
2026-04-18 22:56:13 +0200

    made a collection of asteroids :)

--- Assets/Scripts/MovingAsteroid.cs
+++ Assets/Scripts/MovingAsteroid.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class MovingAsteroid : BaseObstacle
+public class MovingAsteroid : StaticAsteroid
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
