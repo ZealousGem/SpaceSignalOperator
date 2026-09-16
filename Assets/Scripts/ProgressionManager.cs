e25663d27419d561daa2311101098a2a251e4550
Jordan Oos <141826168+ZealousGem@users.noreply.github.com>
2026-09-16 15:50:15 +0200

    mad file script will add file and test now

--- Assets/Scripts/ProgressionManager.cs
+++ Assets/Scripts/ProgressionManager.cs
@@ -1,4 +1,12 @@
 using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public struct ProgressionData
+{
+   public int ProgressionCounter {get; set;}
+   public List<bool> IsAvailable {get; set;}
+}
 
 public class ProgressionManager : Singleton<ProgressionManager>
 {
@@ -6,11 +14,74 @@ public class ProgressionManager : Singleton<ProgressionManager>
    public int MaxCount = 0;
    private int ProgressionCounter;
    private List<bool> IsAvailable = new List<bool>();
+   private ProgressionData data = new ProgressionData();
+   private const string fileName = "ProgressionData.json";
+   private string persistentPath => Path.Combine(Application.persistentDataPath, fileName);
 
     public override void Awake()
     {
         base.Awake();
-        LoadDefaults();
+        DataInFile();
+    }
+
+    private void DataInFile()
+    {
+      if (File.Exists(persistentPath))
+      {
+        try
+        {
+            string json = File.ReadAllText(persistentPath);
+            if (string.IsNullOrEmpty(json)){setParameters(); return;} 
+            
+            data = JsonUtility.FromJson<ProgressionData>(json);
+            setParameters();
+          //  Debug.Log("File found " + Application.persistentDataPath);
+            return;
+        } 
+
+        catch 
+        {
+             Debug.Log("File Corrupted");
+             setParameters();
+            return;
+            // Handle corrupted JSON
+        }
+      }
+         Debug.Log("File not found");
+         setParameters();
+        
+         // No file found in either location
+    }
+
+    private void setParameters()
+    {
+        if (!EqualityComparer<ProgressionData>.Default.Equals(data, default))
+        {
+            ProgressionCounter = data.ProgressionCounter;
+            IsAvailable = data.IsAvailable;
+        }
+
+        else
+        {
+            Debug.Log("defaults");
+            LoadDefaults();
+        }
+
+        setData();
+    } 
+
+    private void setData()
+    {
+        data.ProgressionCounter = ProgressionCounter;
+        data.IsAvailable = IsAvailable;
+         
+
+        if (File.Exists(persistentPath))
+        {
+           string json = JsonUtility.ToJson(data, true);
+           File.WriteAllText(persistentPath, json);
+        }
+       
     }
 
    private void LoadDefaults()
@@ -51,5 +122,7 @@ public class ProgressionManager : Singleton<ProgressionManager>
                 break;
             }
         }
+
+        setData();
     }
 }
